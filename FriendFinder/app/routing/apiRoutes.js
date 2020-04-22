947ee2ccc2a8bef6eec7e56dac874e5f78652ed9
DJ1209 <59895036+DJ1209@users.noreply.github.com>
2020-04-22 00:10:44 -0500

    Add modification

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+//Required dependencies
+var path = require('path');
+
+
+var friend = require('../data/friends.js');
+
+// Export API routes
+module.exports = function(app) {
+
+    app.get('/api/friend', function(req, res) {
+        res.json(friend);
+    });
+
+    // Add friend entry
+    app.post('/api/friend', function(req, res) {
+
+        var userInput = req.body;
+        var userResponses = userInput.scores;
+        var matchedName = '';
+        var matchedPhoto = '';
+        var checkNum = 10000;
+
+
+        for (var i = 0; i < friend.length; i++) {
+
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friend[i].scores[j] - userResponses[j]);
+            }
+
+            if (diff < checkNum) {
+                checkNum = diff;
+                matchedName = friend[i].name;
+                matchedPhoto = friend[i].photo;
+            }
+        }
+
+
+        friend.push(userInput);
+
+        res.json({ status: 'OK', matchedName: matchedName, matchedPhoto: matchedPhoto });
+    });
+};
